696cf887e66891411136c172b92b0c0a1826a90f
j10 <you@example.com>
2023-11-07 08:29:18 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-string imie;
-string odp;
+string name;
+string answ;
+int points = 0;
+
+void startAQuestion(string question, string answerA, string answerB, string answerC, string correctAnswerPrefix);
 
 int main()
 {
     //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
+    HANDLE hndle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hndle, 12);
     cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
+    cin >> name;
 
     system("cls");
     cout<< "3";
@@ -27,28 +29,37 @@ int main()
     Sleep(1000);
     system("cls");
 
-    cout << imie << " witaj w tescie z programowania\n";
+    startAQuestion("wybierz od a do c moze zgadniesz 1.", "odp a", "odp b", "odp c", "c");
+    startAQuestion("wybierz od a do c moze zgadniesz 2.", "odp a", "odp b", "odp c", "b");
+    startAQuestion("wybierz od a do c moze zgadniesz 3.", "odp a", "odp b", "odp c", "c");
+    startAQuestion("wybierz od a do c moze zgadniesz 4.", "odp a", "odp b", "odp c", "a");
+    startAQuestion("wybierz od a do c moze zgadniesz 5.", "odp a", "odp b", "odp c", "b");
 
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
+    printf("===================== LOG =====================\nImie: %s\nPunkty: %d\n===================== LOG =====================", name.c_str(), points);
+
+
+    return 0;
+}
+
+void startAQuestion(string question, string answerA, string answerB, string answerC, string correctAnswerPrefix){
+        cout << name << " witaj w tescie z programowania\n";
+
+    cout << question<<endl;
+    cout << "a) "<<answerA<<endl;
+    cout << "b) "<<answerB<<endl;
+    cout << "c) "<<answerC<<endl;
     cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
+    cin >> answ;
+    if(answ == correctAnswerPrefix)
     {
         cout << "Poprawna odpowiedz!";
-        pkt++;
+        points++;
     }
     else
     {
-        cout << "Zle! Jest to jezyk programowania!";
+        cout << "Zle!";
 
     }
     Sleep(3000);
     system("cls");
-
-
-
-    return 0;
 }
